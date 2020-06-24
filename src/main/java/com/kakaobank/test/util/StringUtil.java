df2d18f2f74e250b9c545a849150679c4f3c8901
NamgungMin <min772a@naver.com>
2020-06-24 23:20:52 +0900

    로그 파싱 method 추가 - parseLog

--- src/main/java/com/kakaobank/test/util/StringUtil.java
+++ src/main/java/com/kakaobank/test/util/StringUtil.java
@@ -8,6 +8,9 @@ import java.util.HashMap;
 
 public class StringUtil {
 
+    public StringUtil() {
+    }
+
     public HashMap<String, String> jsonParser(String s) {
         Gson gson = new Gson();
         return gson.fromJson(s, HashMap.class);
@@ -39,6 +42,45 @@ public class StringUtil {
             }
         }
         return cutResult;
+    }
+
+    private String[] mobileColumns = {"logId", "userId","logDt","deviceId","remoteIp","osType","osVersion"};
+    private String[] bankingColumns = {"transactionDt", "logId", "logDt", "resultCode", "userId", "transactionContent"};
+    public HashMap<String, String> parseLog(String log, String type) throws Exception {
+        HashMap<String, String> resultMap = new HashMap<>();
+        String temp[] = log.split(" ");
+        int index=0;
+        Exception e;
+        if("mobile".equals(type)) {
+            if(mobileColumns.length != temp.length) {
+                e = new Exception("Wrong mobile log : " + log);
+                throw e;
+            }
+            for(String key:mobileColumns) {
+                resultMap.put(key, temp[index]);
+                index++;
+            }
+        } else if("banking".equals(type)) {
+            if(bankingColumns.length > temp.length) {
+                e = new Exception("Wrong banking log : " + log);
+                throw e;
+            }
+            for(String key:bankingColumns) {
+                if("transactionContent".equals(key)) {
+                    StringBuilder sb = new StringBuilder();
+                    String delimeter ="";
+                    for (int i = index;i<temp.length;i++) {
+                        sb.append(delimeter).append(temp[i]);
+                        delimeter=" ";
+                    }
+                    resultMap.put(key, sb.toString());
+                } else {
+                    resultMap.put(key, temp[index]);
+                }
+                index++;
+            }
+        }
+        return resultMap;
     }
 
 }

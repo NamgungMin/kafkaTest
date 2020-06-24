aabe8d526d9288400f29e74a24ecc4c2c885732d
NamgungMin <min772a@naver.com>
2020-06-24 18:40:00 +0900

    토픽 리스트 변경 및 추가 - test -> banking / mobile KUDU의 value max size(64kb)이 넘게 들어오는 값 분할 저장 추가

--- src/main/java/com/kakaobank/test/consumer/SimpleKafkaConsumer.java
+++ src/main/java/com/kakaobank/test/consumer/SimpleKafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.kakaobank.test.consumer;
 
+import com.kakaobank.test.util.Log;
 import com.kakaobank.test.util.StringUtil;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -11,24 +12,28 @@ import org.apache.kudu.client.KuduSession;
 import org.apache.kudu.client.KuduTable;
 import sun.reflect.annotation.ExceptionProxy;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.security.Key;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Properties;
+import java.util.*;
 
 public class SimpleKafkaConsumer {
+
+    private static Log logger = Log.getInstance();
+    private static int cutLen = 1024*8;
+
     public void consumer() {
         Properties props = new Properties();
         props.put("bootstrap.servers", "10.162.165.39:9092,10.162.165.40:9092,10.162.165.41:9092");
         props.put("session.timeout.ms", "10000");
         props.put("group.id", "test");
+        props.put("compression.type", "gzip");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");    // key deserializer
         props.put("value.deserializer", "io.confluent.kafka.serializers.KafkaAvroDeserializer");  // value deserializer
         props.put("schema.registry.url", "http://10.162.165.39:8081");
-        KafkaConsumer<String, GenericRecord> consumer = new KafkaConsumer<String, GenericRecord>(props);
-        consumer.subscribe(Arrays.asList("test"));
+        KafkaConsumer<String, GenericRecord> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("mobile", "banking"));
         KuduClient kuduClient =new KuduClient.KuduClientBuilder("pmda-reml-t1003.svr.toastmaker.net:7051").build();
         StringUtil su = new StringUtil();
         HashMap<String, String> consumStr;
@@ -36,23 +41,66 @@ public class SimpleKafkaConsumer {
             ConsumerRecords<String, GenericRecord> records = consumer.poll(Duration.ofMillis(500));
             for (ConsumerRecord<String, GenericRecord> record : records) {
                 String s = record.topic();
-                if ("test".equals(s)) {
-                    System.out.println(record.value());
+                logger.info( s +"/" +record.value().toString(), SimpleKafkaConsumer.class);
+                if ("mobile".equals(s)) {
                     consumStr = su.jsonParser(record.value().toString());
                     try {
                         KuduSession session = kuduClient.newSession();
-                        KuduTable table = kuduClient.openTable("impala::test.test_mobile");
+                        KuduTable table = kuduClient.openTable("impala::test.mobile");
 
                         Insert insert = table.newInsert();
-                        for( String key : consumStr.keySet() ){
+                        for (String key : consumStr.keySet()) {
                             insert.getRow().addString(key.toLowerCase(), consumStr.get(key));
                         }
                         session.apply(insert);
 
-                    } catch (Exception e){
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
+                } else if ("banking".equals(s)) {
+                    consumStr = su.jsonParser(record.value().toString());
+                    try {
+                        KuduSession session = kuduClient.newSession();
+                        KuduTable table = kuduClient.openTable("impala::test.banking");
 
+                        Insert insert = table.newInsert();
+                        String logId = consumStr.get("logId");
+                        for (String key : consumStr.keySet()) {
+                            logger.info(key.toLowerCase()+" / " + consumStr.get(key), SimpleKafkaConsumer.class);
+
+                            if("transactioncontent".equals(key.toLowerCase())) {
+                                StringBuilder sb = new StringBuilder();
+                                String transactioncontent = URLDecoder.decode(consumStr.get(key),"utf-8");
+                                ArrayList<String> splitStr = su.getMaxByteString(transactioncontent, cutLen);
+                                KuduSession session_detail = kuduClient.newSession();
+                                KuduTable table_detail = kuduClient.openTable("impala::test.banking_transactioncontent");
+
+                                String sublogid;
+                                String delimeter = "";
+                                int index = 0;
+                                for (String str : splitStr) {
+                                    sublogid = logId+"_"+index;
+                                    Insert insert_detail = table_detail.newInsert();
+                                    insert_detail.getRow().addString("sublogid", sublogid);
+                                    insert_detail.getRow().addString("logid", logId);
+                                    insert_detail.getRow().addString("transactioncontent", str);
+                                    session_detail.apply(insert_detail);
+                                    sb.append(delimeter).append(sublogid);
+                                    delimeter = ",";
+                                    index++;
+                                }
+                                logger.info("transactioncontentid / " + sb.toString(), SimpleKafkaConsumer.class);
+                                insert.getRow().addString("transactioncontentid", sb.toString());
+                            } else {
+                                insert.getRow().addString(key.toLowerCase(), consumStr.get(key));
+                            }
+                        }
+
+                        session.apply(insert);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 } else {
                     throw new IllegalStateException("get message on topic " + record.topic());
                 }

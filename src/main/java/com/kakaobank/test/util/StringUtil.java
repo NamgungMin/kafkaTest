17b175df02d45bb68af369f2e774920691fa192a
NamgungMin <min772a@naver.com>
2020-06-24 16:09:02 +0900

    String byte 단위로 분리하는 method 추가

--- src/main/java/com/kakaobank/test/util/StringUtil.java
+++ src/main/java/com/kakaobank/test/util/StringUtil.java
@@ -3,13 +3,42 @@ package com.kakaobank.test.util;
 import com.google.gson.Gson;
 import org.apache.kudu.Hash;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class StringUtil {
 
     public HashMap<String, String> jsonParser(String s) {
         Gson gson = new Gson();
-        HashMap<String, String> result = gson.fromJson(s, HashMap.class);
-        return result;
+        return gson.fromJson(s, HashMap.class);
     }
+
+    public ArrayList<String> getMaxByteString(String str, int cutLen)
+    {
+        ArrayList<String> cutResult = new ArrayList<>();
+        if( str.getBytes().length <= cutLen) {
+            cutResult.add(str);
+            return cutResult;
+        } else {
+            StringBuilder sb = new StringBuilder();
+            int cutNum = 0;
+            String curChar;
+
+            for (char ch : str.toCharArray()) {
+                cutNum += String.valueOf(ch).getBytes().length;
+                if(cutNum > cutLen) {
+                    cutResult.add(sb.toString());
+                    sb.setLength(0);
+                    cutNum = 0;
+                } else {
+                    sb.append(ch);
+                }
+            }
+            if(sb.length() > 0) {
+                cutResult.add(sb.toString());
+            }
+        }
+        return cutResult;
+    }
+
 }

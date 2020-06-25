e8c821510a1c79a1e778b3195ba912bbcb2b575c
NamgungMin <min772a@naver.com>
2020-06-25 22:44:36 +0900

    File tail kafka producer로 변경

--- src/main/java/com/kakaobank/test/KafkaTest.java
+++ src/main/java/com/kakaobank/test/KafkaTest.java
@@ -1,23 +1,46 @@
 package com.kakaobank.test;
+import com.kakaobank.test.util.FileLogTail;
 import com.kakaobank.test.util.Log;
 import com.kakaobank.test.producer.SimpleKafkaProducer;
 import com.kakaobank.test.consumer.SimpleKafkaConsumer;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class KafkaTest{
 
     public static void main(String[] args){
-        System.out.println("start");
         Log logger = Log.getInstance();
-        logger.info("test",KafkaTest.class);
-        //String mode = args[0];
+        String mode = "producer";
+        String type = "banking";
+        int interval = 100;
+        String logFilePath = "C:\\Users\\admin\\IdeaProjects\\kakao\\src\\log_data\\banking.log";
 
-        // kafka producser Test
-        SimpleKafkaProducer simpleKafkaProducer = new SimpleKafkaProducer();
-        logger.info("sendSync",KafkaTest.class);
-        simpleKafkaProducer.sendSync();
+        if(args.length > 0) {
+            mode = args[0];
+            if("producer".equals(mode)) {
+                type = args[1];
+                logFilePath = args[2];
+            }
+        }
 
-        // kafka consumer Test
-//        SimpleKafkaConsumer simpleKafkaConsumer = new SimpleKafkaConsumer();
-//        simpleKafkaConsumer.consumer();
+        logger.info("mode : " + mode + " / type : " + type,KafkaTest.class);
+        if( "producer".equals(mode)) {
+            // kafka producer
+            SimpleKafkaProducer simpleKafkaProducer = new SimpleKafkaProducer();
+            logger.info("sendSync",KafkaTest.class);
+            try {
+                ExecutorService logExecutor = Executors.newFixedThreadPool(4);
+                FileLogTail flt = new FileLogTail(logFilePath,interval,type);
+                logExecutor.execute(flt);
+            } catch (Exception e) {
+                logger.error(e.toString(), KafkaTest.class);
+            }
+        } else if ( "consumer".equals(mode)) {
+            // kafka consumer
+            SimpleKafkaConsumer simpleKafkaConsumer = new SimpleKafkaConsumer();
+            simpleKafkaConsumer.consumer();
+        }
+
     }
 }
